354ea28d09d8ab87e4806bf913b9a4519dbd27bd
wevertoum <wevertonmixalt@gmail.com>
2024-10-11 15:46:07 -0300

    componentizando e trocando estrategia de blur da imagem

---
+++ src/components/NavigationOnboarding.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ArrowIcon from "./icons/ArrowIcon";
+
+interface Props {
+  current: number;
+  setCurrent: React.Dispatch<React.SetStateAction<number>>;
+  itens: PageCarousel[];
+  theme?: string;
+}
+
+const NavigationOnboarding: React.FC<Props> = ({
+  current,
+  setCurrent,
+  itens,
+  theme,
+}) => {
+  const isPrevDisabled = current === 0;
+  const isNextDisabled = current === itens.length - 1;
+
+  return (
+    <div className="flex items-center justify-around mt-6 w-[154px] h-[64px] shadow-lg py-2 px-4 rounded-[16px] bg-white dark:bg-laborit-navigator-dark">
+      <button onClick={() => setCurrent(current - 1)} disabled={isPrevDisabled}>
+        <ArrowIcon direction="left" disabled={isPrevDisabled} theme={theme} />
+      </button>
+      <div className="h-6 w-px bg-laborit-separator dark:laborit-separator-dark"></div>
+      <button onClick={() => setCurrent(current + 1)} disabled={isNextDisabled}>
+        <ArrowIcon direction="right" disabled={isNextDisabled} theme={theme} />
+      </button>
+    </div>
+  );
+};
+
+export default NavigationOnboarding;
--- src/components/PageCarousel.tsx
+++ src/components/PageCarousel.tsx
@@ -2,9 +2,8 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { useTheme } from "next-themes";
-import ArrowIcon from "./icons/ArrowIcon";
-import SelectedDotIcon from "./icons/SelectedDotIcon";
-import DefaultDotIcon from "./icons/DefaultDotIcon";
+import NavigationOnboarding from "./NavigationOnboarding";
+import RenderDotsOnboarding from "./RenderDotsOnboarding";
 
 interface Props {
   itens: PageCarousel[];
@@ -15,52 +14,22 @@ const PageCarousel: React.FC<Props> = ({ itens }) => {
   const [current, setCurrent] = useState(0);
   const { theme } = useTheme();
 
-  const RenderDots = () => {
-    return (
-      <div className="flex justify-between my-4 items-center w-[60px]">
-        {itens.map((_, index) => (
-          <div key={index}>
-            {current === index ? (
-              <SelectedDotIcon theme={theme} size={17.68} />
-            ) : (
-              <DefaultDotIcon theme={theme} />
-            )}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  const Navigation = () => {
-    const isPrevDisabled = current === 0;
-    const isNextDisabled = current === itens.length - 1;
-
-    return (
-      <div className="flex items-center justify-around mt-4 w-[154px] h-[64px] shadow-lg py-2 px-4 rounded-[16px] bg-white dark:bg-laborit-navigator-dark">
-        <button
-          onClick={() => setCurrent(current - 1)}
-          disabled={isPrevDisabled}
-        >
-          <ArrowIcon direction="left" disabled={isPrevDisabled} theme={theme} />
-        </button>
-        <div className="h-6 w-px bg-laborit-separator dark:laborit-separator-dark"></div>
-        <button
-          onClick={() => setCurrent(current + 1)}
-          disabled={isNextDisabled}
-        >
-          <ArrowIcon
-            direction="right"
-            disabled={isNextDisabled}
-            theme={theme}
-          />
-        </button>
-      </div>
-    );
-  };
-
   return (
     <div className="flex flex-col items-center h-full">
-      <div className="relative  w-[362px] h-[455px] mt-[95px]">
+      <div className="relative w-[362px] h-[455px] mt-[95px]">
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            alt={`${itens[current].title}-shadow`}
+            src={itens[current].image}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: "cover",
+              borderRadius: "33.05px",
+              filter: "blur(40px) brightness(0.7)",
+            }}
+          />
+        </div>
         <Image
           alt={itens[current].title}
           src={itens[current].image}
@@ -69,13 +38,12 @@ const PageCarousel: React.FC<Props> = ({ itens }) => {
           style={{
             objectFit: "cover",
             borderRadius: "33.05px",
-            filter: "drop-shadow(0 0 70px #75717181)",
           }}
           className="px-4 pb-4"
         />
       </div>
 
-      <RenderDots />
+      <RenderDotsOnboarding current={current} itens={itens} theme={theme} />
       <div className="w-80">
         <h1 className="text-center text-[33.9px] text-laborit-text-title dark:text-white font-bold">
           {itens[current].title}
@@ -85,7 +53,12 @@ const PageCarousel: React.FC<Props> = ({ itens }) => {
         </p>
       </div>
 
-      <Navigation />
+      <NavigationOnboarding
+        current={current}
+        setCurrent={setCurrent}
+        itens={itens}
+        theme={theme}
+      />
     </div>
   );
 };
