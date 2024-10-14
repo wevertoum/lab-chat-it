c48e445ac148005e2b7d958a35ce41a3369cbac2
wevertoum <wevertonmixalt@gmail.com>
2024-10-14 12:37:11 -0300

    ajustes na formatacao do codigo

--- src/components/CopyInviteCode.tsx
+++ src/components/CopyInviteCode.tsx
@@ -11,7 +11,10 @@ interface Props {
 const CopyInviteCode: React.FC<Props> = ({ code, onCopy, theme }) => {
   return (
     <div
-      className={`h-[74px] w-[344px] flex items-center justify-between rounded-[15px] border-[2px] border-laborit-dark-gray dark:border-white p-4 bg-laborit-input-edit-background dark:bg-laborit-dark-gray`}
+      className={`h-[74px] w-[344px] flex items-center justify-between 
+        rounded-[15px] border-[2px] border-laborit-dark-gray 
+        dark:border-white p-4 bg-laborit-input-edit-background
+        dark:bg-laborit-dark-gray`}
     >
       <span className="font-semibold text-[17px] text-laborit-dark-gray dark:text-white ml-2">
         {code}
